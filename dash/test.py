f6ac0e6fb4e98e996e20e86690915675f2e23459
Reid Fleishman <rf377@cornell.edu>
2023-02-06 15:51:47 -0500

    Create test.py

---
+++ dash/test.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+import requests
+
+url = "https://portal-api.emcs.cucloud.net/query"
+data = {
+    "range": {
+        "from": str(datetime.now() - timedelta(days=30)),
+        "to": str(datetime.now())
+    },
+    "targets": [
+        {
+            "payload": {
+                "additional": [
+                    "noagg",
+                ]
+            },
+            "target": "GameFarmRoadWeatherStation.TAVG_H_F",
+        }
+    ]
+}
+request = requests.post(url, json=data)
+print(request)
