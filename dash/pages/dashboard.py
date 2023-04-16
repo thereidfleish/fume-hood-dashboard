e1276058638a8eac2a6087854612c57f54577bb2
Reid Fleishman <rf377@cornell.edu>
2023-04-16 18:19:53 -0400

    finish pages

--- dash/pages/dashboard.py
+++ dash/pages/dashboard.py
@@ -13,118 +13,118 @@ app = Dash(__name__)
 
 dash.register_page(__name__)
 
-layout = html.Div(className="cols_wrapper", children=[
-    dcc.Location(id='url', refresh=False),  # URL location component
-    html.Div(className="col_small", children=[
-        dash_treeview_antd.TreeView(
-            id='input',
-            multiple=False,
-            checkable=False,
-            checked=['0-0-1'],
-            selected=[],
-            expanded=['0'],
-            data={
-                'title': 'Biotech',
-                'key': 'biotech',
-                'children': [{
-                    'title': 'Floor 1',
-                    'key': 'biotech/floor_1',
-                    'children': [
-                        {'title': 'Lab 1', 'key': 'biotech/floor_1/lab_1'},
-                        {'title': 'Lab 2', 'key': 'biotech/floor_1/lab_2'},
-                        {'title': 'Lab 3', 'key': 'biotech/floor_1/lab_3'},
-                    ],
-                }]}
-        )
-    ]),
-
-    html.Div(className="col", children=[
-        html.Div(className="cols_wrapper", children=[
-            html.Div(className="col", children=[
-                html.H1(id='title')
-            ]),
-
-            html.Div(className="col", children=[
-                html.Label('Metric'),
-                dcc.Dropdown(["BTU"],
-                             "BTU", id="metric_selector"),
-            ]),
-
-            html.Div(className="col", children=[
-                html.Label('Date Range'),
-                dcc.Dropdown(["Last day", "Last week", "Last month"],
-                             "Last week", id="date_selector"),
-            ])
+def layout(building=None, floor=None, lab=None, **other_unknown_query_strings):
+    return html.Div(className="cols_wrapper", children=[
+        dcc.Location(id='url', refresh=False),  # URL location component
+        html.Div(className="col_small", children=[
+            dash_treeview_antd.TreeView(
+                id='input',
+                multiple=False,
+                checkable=False,
+                checked=[],
+                selected=[],
+                expanded=["?building=biotech"],
+                data={
+                    'title': 'Biotech',
+                    'key': '?building=biotech',
+                    'children': [{
+                        'title': 'Floor 1',
+                        'key': '?building=biotech&floor=1',
+                        'children': [
+                            {'title': 'Lab 1', 'key': '?building=biotech&floor=1&lab=1'},
+                            {'title': 'Lab 2', 'key': '?building=biotech&floor=1&lab=2'},
+                            {'title': 'Lab 3', 'key': '?building=biotech&floor=1&lab=3'},
+                        ],
+                    }]}
+            )
         ]),
 
-
-        html.P(id='breadcrumb'),
-
-        html.Div(id='output-selected'),
-
-        html.Div(className="cols_wrapper", children=[
-            html.Div(className="col", children=[
-                html.H3("Featured Rankings"),
-                
-                # Rankings box
-                html.Div(className="box", children=[
-                    html.H2("3rd Place"),
-                    html.H4("on Olin Floor 3"),
-                    html.P("Least avg. energy when unoccupied (2000 BTU/hr)")
+        html.Div(className="col", children=[
+            html.Div(className="cols_wrapper", children=[
+                html.Div(className="col", children=[
+                    html.H1(', '.join(filter(None, (building, floor, lab))))
                 ]),
 
-                html.Div(className="box", children=[
-                    html.H2("1st Place"),
-                    html.H4("on Olin Floor 3"),
-                    html.P("Least avg. time when unoccupied (0 min/hr)")
+                html.Div(className="col", children=[
+                    html.Label('Metric'),
+                    dcc.Dropdown(["BTU"],
+                                "BTU", id="metric_selector"),
                 ]),
+
+                html.Div(className="col", children=[
+                    html.Label('Date Range'),
+                    dcc.Dropdown(["Last day", "Last week", "Last month"],
+                                "Last week", id="date_selector"),
+                ])
             ]),
 
-            html.Div(className="col", children=[
-                html.H3("Graphs"),
 
-                dcc.Loading(
-                    id="is-loading",
-                    children=[
-                        dcc.Graph(
-                            id="occ_graph",
-                            # figure=fig
-                        )],
-                    type="circle"
-                )
+            html.P(id='breadcrumb'),
+
+            html.Div(id='output-selected'),
+
+            html.Div(className="cols_wrapper", children=[
+                html.Div(className="col", children=[
+                    html.H3("Featured Rankings"),
+                    
+                    # Rankings box
+                    html.Div(className="box", children=[
+                        html.H2("3rd Place"),
+                        html.H4("on Olin Floor 3"),
+                        html.P("Least avg. energy when unoccupied (2000 BTU/hr)")
+                    ]),
+
+                    html.Div(className="box", children=[
+                        html.H2("1st Place"),
+                        html.H4("on Olin Floor 3"),
+                        html.P("Least avg. time when unoccupied (0 min/hr)")
+                    ]),
+                ]),
+
+                html.Div(className="col", children=[
+                    html.H3("Graphs"),
+
+                    dcc.Loading(
+                        id="is-loading",
+                        children=[
+                            dcc.Graph(
+                                id="occ_graph",
+                                # figure=fig
+                            )],
+                        type="circle"
+                    )
+                ])
             ])
-        ])
-    ]),
-
+        ]),
 ])
 
-@callback(Output('title', 'children'),
-              [Input('input', 'selected')])
-def update_title(selected):
-    if selected:
-        selected_item = selected[0]  # Assuming single selection
-        return selected_item
-    else:
-        return 'Biotech' # Default value
+# @callback(Output('title', 'children'),
+#               [Input('input', 'selected')])
+# def update_title(selected):
+#     if selected:
+#         selected_item = selected[0]  # Assuming single selection
+#         return selected_item
+#     else:
+#         return 'Biotech' # Default value
 
 
-@callback(Output('breadcrumb', 'children'),
-              [Input('input', 'selected')])
-def update_breadcrumb(selected):
-    if selected:
-        selected_item = selected[0]  # Assuming single selection
+# @callback(Output('breadcrumb', 'children'),
+#               [Input('input', 'selected')])
+# def update_breadcrumb(selected):
+#     if selected:
+#         selected_item = selected[0]  # Assuming single selection
         
-        if selected_item == 'Biotech':
-            breadcrumb_text = html.A('Biotech', href='/biotech')
-        elif selected_item == 'Floor 1':
-            breadcrumb_text = html.A('Biotech', href='/biotech'), ' / ', html.A('Floor 1', href='/floor1')
-        else:
-            breadcrumb_text = html.A('Biotech', href='/biotech'), ' / ', html.A('Floor 1', href='/floor1'), ' / ', html.A(selected_item, href='/' + selected_item)
+#         if selected_item == 'Biotech':
+#             breadcrumb_text = html.A('Biotech', href='/biotech')
+#         elif selected_item == 'Floor 1':
+#             breadcrumb_text = html.A('Biotech', href='/biotech'), ' / ', html.A('Floor 1', href='/floor1')
+#         else:
+#             breadcrumb_text = html.A('Biotech', href='/biotech'), ' / ', html.A('Floor 1', href='/floor1'), ' / ', html.A(selected_item, href='/' + selected_item)
         
-        return html.P(breadcrumb_text)
-    else:
-        breadcrumb_text = html.A('Biotech', href='/biotech')  # Default value
-        return html.P(breadcrumb_text)
+#         return html.P(breadcrumb_text)
+#     else:
+#         breadcrumb_text = html.A('Biotech', href='/biotech')  # Default value
+#         return html.P(breadcrumb_text)
 
 
 # @app.callback(Output("url", "pathname"), [Input("input", "selected_item")])
@@ -161,6 +161,17 @@ def update_breadcrumb(selected):
 # def _display_selected(selected):
 #     return 'You have checked {}'.format(selected)
 
+clientside_callback(
+    """
+    function(input) {
+        console.log(input[0]);
+        window.open(`/pages/dashboard${input[0]}`, "_self");
+        return input[0];
+    }
+    """,
+    Output('output-selected', 'children'),
+    Input('input', 'selected'), prevent_initial_call=True
+)
 
 @callback(
     Output("occ_graph", "figure"),
@@ -245,18 +256,6 @@ def update_graph(date):
     print(occ_data)
     occ_fig = px.bar(occ_data)
     return occ_fig
-
-clientside_callback(
-    """
-    function(input) {
-        console.log(input[0]);
-        window.open(`/pages/dashboard/${input[0]}`, "_self");
-        return input[0];
-    }
-    """,
-    Output('output-selected', 'children'),
-    Input('input', 'selected'), prevent_initial_call=True
-)
 
 # if __name__ == '__main__':
 #     app.run_server(debug=True)
